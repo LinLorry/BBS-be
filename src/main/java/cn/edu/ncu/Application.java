3cb83e42d8b59b35545d408fc058ee0929bdcf43
LinLorry <lin864464995@163.com>
2019-12-09 12:53:35 +0800

    :tada: Init spring boot application.

---
+++ src/main/java/cn/edu/ncu/Application.java
@@ -0,0 +1,16 @@
+package cn.edu.ncu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Application.
+ * @author lorry
+ * @author lin864464995@163.com
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
