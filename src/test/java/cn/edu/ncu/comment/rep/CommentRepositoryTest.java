b295c5b992635fc4a3c404f05479b90f016f8463
LinLorry <lin864464995@163.com>
2019-12-09 23:58:29 +0800

    :white_check_mark: Add comment repository test.

---
+++ src/test/java/cn/edu/ncu/comment/rep/CommentRepositoryTest.java
@@ -0,0 +1,93 @@
+package cn.edu.ncu.comment.rep;
+
+import cn.edu.ncu.comment.model.Comment;
+import cn.edu.ncu.comment.model.CommentKey;
+import cn.edu.ncu.topic.model.Topic;
+import cn.edu.ncu.topic.rep.TopicRepository;
+import cn.edu.ncu.user.model.User;
+import cn.edu.ncu.user.rep.UserRepository;
+import net.bytebuddy.utility.RandomString;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.sql.Timestamp;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentRepositoryTest {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @Transactional
+    public void save() {
+        Topic topic = topicRepository.findById(1L).orElseThrow(NoSuchElementException::new);
+        User user = userRepository.findById(1L).orElseThrow(NoSuchElementException::new);
+
+        Comment comment = new Comment();
+        comment.setTopic(topic);
+        comment.setUser(user);
+        comment.setLocation(commentRepository.countAllByTopicAndUser(topic, user) + 1);
+        comment.setContent(RandomString.make());
+        comment.setCreateTime(new Timestamp(System.currentTimeMillis()));
+
+        entityManager.persist(comment);
+        entityManager.flush();
+
+        assertEquals(comment.getTopic().getId(), topic.getId());
+        assertEquals(comment.getUser().getId(), user.getId());
+        System.out.println(comment);
+    }
+
+    @Test
+    public void findById() {
+        CommentKey commentKey = new CommentKey(1, 1L, 1L);
+        Comment comment = commentRepository.findById(commentKey).orElseThrow(NoSuchElementException::new);
+
+        assertEquals(comment.getLocation(), new Integer(1));
+        assertEquals(comment.getTopic().getId(), new Long(1));
+        assertEquals(comment.getUser().getId(), new Long(1));
+
+        System.out.println(comment);
+    }
+
+    @Test
+    public void findAll() {
+        Iterable<Comment> iterator = commentRepository.findAll();
+
+        assertNotNull(iterator);
+        iterator.forEach(System.out::println);
+    }
+
+    @Test
+    public void countAllByTopicAndUser() {
+        Topic topic = new Topic();
+        User user = new User();
+
+        topic.setId(1L);
+        user.setId(1L);
+
+        Integer result = commentRepository.countAllByTopicAndUser(topic, user);
+
+        assertNotNull(result);
+        System.out.println(result);
+    }
+}
