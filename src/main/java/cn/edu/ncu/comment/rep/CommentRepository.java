75e0584a922545ffa523106e85979956ce3aa948
LinLorry <lin864464995@163.com>
2019-12-10 16:14:50 +0800

    :sparkles: Update comment repository and add get max location by topic id.

--- src/main/java/cn/edu/ncu/comment/rep/CommentRepository.java
+++ src/main/java/cn/edu/ncu/comment/rep/CommentRepository.java
@@ -3,11 +3,21 @@ package cn.edu.ncu.comment.rep;
 import cn.edu.ncu.comment.model.Comment;
 import cn.edu.ncu.comment.model.CommentKey;
 import cn.edu.ncu.topic.model.Topic;
-import cn.edu.ncu.user.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Comment Repository
+ * @author lorry
+ * @author lin864464995@163.com
+ */
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, CommentKey> {
-    Integer countAllByTopicAndUser(Topic topic, User user);
+    @Query("SELECT max(comment.location) FROM Comment comment WHERE comment.topic.id = ?1")
+    Integer getMaxLocationByTopicId(Long topicId);
+
+    Page<Comment> findAllByTopic(Topic topic, Pageable pageable);
 }
