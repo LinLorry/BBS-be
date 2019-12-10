94316306e2eb04c02b8d721c8ef6ed813e9a3893
lyw <2354640364@qq.com>
2019-12-10 23:27:16 +0800

    :sparkles: add topic repository

--- src/main/java/cn/edu/ncu/topic/rep/TopicRepository.java
+++ src/main/java/cn/edu/ncu/topic/rep/TopicRepository.java
@@ -3,7 +3,17 @@ package cn.edu.ncu.topic.rep;
 import cn.edu.ncu.topic.model.Topic;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
+/**
+ * Topic Repository
+ * @author LYW
+ * @author 2354640364@qq.com
+ * @see org.springframework.data.repository.CrudRepository
+ */
 @Repository
 public interface TopicRepository extends CrudRepository<Topic, Long> {
+    Optional<Topic> findById(long id);
+
+    boolean existsById(long id);
 }
