6fc4886357c835ae52c2953880e5a720191efa56
linzongzhu <2353506019@qq.com>
2019-12-10 16:24:40 +0800

    Add toptopic service

--- src/main/java/cn/edu/ncu/topic/rep/TopTopicRepository.java
+++ src/main/java/cn/edu/ncu/topic/rep/TopTopicRepository.java
@@ -4,6 +4,12 @@ import cn.edu.ncu.topic.model.TopTopic;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TopTopicRepository extends CrudRepository<TopTopic, Long> {
+    void deleteTopTopicByTopicId(Long topicId);
+    List<TopTopic> findAll();
+    Optional<TopTopic> findByTopicId(Long id);
 }
