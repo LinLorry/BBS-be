4181d898bc9dacb03d75a545c2d665eb88d73495
linzongzhu <2353506019@qq.com>
2019-12-11 14:26:01 +0800

    TopTopic ServiceTest添加测试增加删除查找方法

--- src/test/java/cn/edu/ncu/topic/TopTopicServiceTest.java
+++ src/test/java/cn/edu/ncu/topic/TopTopicServiceTest.java
@@ -17,5 +17,33 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TopTopicServiceTest {
- 
+    @Autowired
+    private TopTopicService topTopicService;
+    @Test
+    public void add() {
+        TopTopic topTopic=new TopTopic();
+        topTopic.setTopicId(2L);
+        topTopicService.add(topTopic);
+        System.out.println(topTopic);
+    }
+
+    @Test
+    @Transactional
+    public void deleteTopTopicByTopicId() {
+        topTopicService.deleteTopTopicByTopicId(1L);
+    }
+
+    @Test
+    public void findAll() {
+        List<TopTopic> topTopics=topTopicService.findAll();
+        for(TopTopic topTopic:topTopics){
+            System.out.println(topTopic);
+        }
+    }
+
+    @Test
+    public void findByTopicId() {
+        TopTopic topTopic=topTopicService.findByTopicId(1L);
+        assertEquals(topTopic.getTopicId(),new Long(1));
+    }
 }
