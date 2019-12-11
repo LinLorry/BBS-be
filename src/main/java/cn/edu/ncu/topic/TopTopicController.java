9c84e022cacb4bdeaf29f89e001591cdb4c12d02
linzongzhu <2353506019@qq.com>
2019-12-11 14:54:18 +0800

    TopTopic Controller  添加 添加删除查找功能

--- src/main/java/cn/edu/ncu/topic/TopTopicController.java
+++ src/main/java/cn/edu/ncu/topic/TopTopicController.java
@@ -23,5 +23,90 @@ public class TopTopicController {
         this.topTopicService = topTopicService;
     }
 
-    
+    /**
+     *
+     * @param request
+     * @return 返回json串，data,status,message
+     * @throws MissingServletRequestParameterException
+     */
+    @ResponseBody
+    @RequestMapping("/add")
+    public JSONObject add(@RequestBody JSONObject request)throws MissingServletRequestParameterException{
+        JSONObject response = new JSONObject();
+        Long id=request.getLong("id");
+        //Topic topic=(Topic)request.get("topic");
+        TopTopic topTopic=new TopTopic();
+        topTopic.setTopicId(id);
+        try {
+            response.put("data", topTopicService.add(topTopic));
+            response.put("status", 1);
+            response.put("message", "Add Success.");
+        } catch (DataIntegrityViolationException e) {
+            if (topTopicService.findByTopicId(id)!=null) {
+                throw e;
+            }
+            response.put("status", 0);
+            response.put("message", "The topic has been placed at the top.");
+        }
+        return response;
+    }
+
+    /**
+     *
+     * @param request
+     * @return 返回status,message
+     */
+    @ResponseBody
+    @Transactional
+    @RequestMapping("/delete")
+    public JSONObject deleteTopTopicByTopicId(@RequestBody JSONObject request){
+        JSONObject response = new JSONObject();
+        Long id=request.getLong("id");
+        try{
+            topTopicService.deleteTopTopicByTopicId(id);
+            response.put("status", 1);
+            response.put("message", "delete toptopic success.");
+
+        }catch (NoSuchElementException e){
+            response.put("status", 0);
+            response.put("message", "The toptopic isn't exist.");
+        }
+        return response;
+    }
+
+    /**
+     *
+     * @return 返回json，包括status,message,data
+     */
+    @ResponseBody
+    @RequestMapping("/find")
+    public JSONObject find() {
+        JSONObject response = new JSONObject();
+        JSONObject data = new JSONObject();
+        List<TopTopic>topTopics=topTopicService.findAll();
+        data.put("result",topTopics);
+        response.put("status", 1);
+        response.put("message", "Get toptopic success.");
+        response.put("data", data);
+        return response;
+    }
+/*
+    @ResponseBody
+    @RequestMapping("/findById")
+    public JSONObject findById(@RequestParam(required = true)Long id){
+        JSONObject response=new JSONObject();
+
+        try{
+            TopTopic topTopic=topTopicService.findByTopicId(id);
+            response.put("data",topTopic);
+            response.put("status",1);
+            response.put("message","Find toptopic by id success");
+
+        }catch (NoSuchElementException e){
+            response.put("status", 0);
+            response.put("message", "The toptopic isn't exist.");
+        }
+        return response;
+
+    }*/
 }
