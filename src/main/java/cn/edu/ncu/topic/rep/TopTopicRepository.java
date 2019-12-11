035a48d75539e83fa5439bb100d3f86e924959ae
linzongzhu <2353506019@qq.com>
2019-12-11 14:19:49 +0800

    TopTopic Repository 增加方法

--- src/main/java/cn/edu/ncu/topic/rep/TopTopicRepository.java
+++ src/main/java/cn/edu/ncu/topic/rep/TopTopicRepository.java
@@ -1,6 +1,7 @@
 package cn.edu.ncu.topic.rep;
 
 import cn.edu.ncu.topic.model.TopTopic;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
