36ccfc01d041855208c1f31724558a885ba8b1c4
linzongzhu <2353506019@qq.com>
2019-12-11 14:25:11 +0800

    TopTopic ServiceTest增加测试

---
+++ src/test/java/cn/edu/ncu/topic/TopTopicServiceTest.java
@@ -0,0 +1,21 @@
+package cn.edu.ncu.topic;
+import cn.edu.ncu.topic.model.TopTopic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TopTopicServiceTest {
+ 
+}
