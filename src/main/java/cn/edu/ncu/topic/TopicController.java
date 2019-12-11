44a8bebae120988739d0fd7a435e65390029e07a
linzongzhu <2353506019@qq.com>
2019-12-11 20:52:24 +0800

    TopicController 修改create

--- src/main/java/cn/edu/ncu/topic/TopicController.java
+++ src/main/java/cn/edu/ncu/topic/TopicController.java
@@ -7,9 +7,17 @@ import cn.edu.ncu.user.model.User;
 import cn.edu.ncu.util.SecurityUtil;
 import cn.edu.ncu.util.TokenUtil;
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * User Controller
@@ -49,6 +57,7 @@ public class TopicController {
      *     "message": "message"
      * }
      */
+    /*
     @ResponseBody
     @PostMapping("/create")
     public JSONObject create(@RequestBody JSONObject request) {
@@ -74,5 +83,44 @@ public class TopicController {
 
         return response;
     }
+*/
+
+    @ResponseBody
+    @PostMapping("/create")
+    public JSONObject create(@RequestBody JSONObject request){
+        JSONObject response = new JSONObject();
+        User user = SecurityUtil.getUser();
+        //Demand demand = new Demand();
+        Topic topic=new Topic();
+        Long id=request.getLong("id");
+        String title=request.getString("title");
+        String content=request.getString("content");
+        Boolean boutique=request.getBoolean("boutique");
+        LocalDate today = LocalDate.now();
+        topic.setCreateTime(Timestamp.valueOf(today.atStartOfDay()));
+        topic.setBoutique(boutique);
+        topic.setCreateUser(user);
+        topic.setContent(content);
+        topic.setTitle(title);
+        topic.setId(id);
+        //topic.setDemand(demand);
+        try{
+            response.put("data", topicService.add(topic));
+            response.put("status", 1);
+            response.put("message", "Create Topic Success.");
+        }catch (Exception e) {
+            response.put("status", 0);
+            if (title == null) {
+                response.put("message", "title can't be null.");
+            } else if (content == null) {
+                response.put("message", "content can't be null.");
+            } else {
+                logger.error(e);
+                response.put("message", "Create Topic Failed.");
+            }
+        }
+        return  response;
+    }
+
 
 }
