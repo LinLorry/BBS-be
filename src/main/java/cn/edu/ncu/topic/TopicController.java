aa1d4b691706014d7da4512b4f485f373c3d2952
lyw <2354640364@qq.com>
2019-12-13 10:44:31 +0800

    :art: Update create topic controller.

--- src/main/java/cn/edu/ncu/topic/TopicController.java
+++ src/main/java/cn/edu/ncu/topic/TopicController.java
@@ -1,19 +1,14 @@
 package cn.edu.ncu.topic;
 
 import cn.edu.ncu.topic.model.Topic;
-import cn.edu.ncu.user.model.User;
 import cn.edu.ncu.util.SecurityUtil;
 import com.alibaba.fastjson.JSONObject;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 import java.sql.Timestamp;
-import java.time.LocalDate;
 import java.util.*;
 
 /**
@@ -24,7 +19,6 @@ import java.util.*;
 @RestController
 @RequestMapping("/api/topic")
 public class TopicController {
-    private final Log logger = LogFactory.getLog(this.getClass());
 
     private final TopicService topicService;
 
@@ -38,10 +32,6 @@ public class TopicController {
      * @param request {
      *      "title": String, not null
      *      "content": String, not null
-     *      "time": Timestamp, not null
-     *      "comment": String
-     *      "demand": Demand
-     *      "user": User
      * }
      * @return if create topic success return {
      *     "status": 1,
@@ -54,45 +44,16 @@ public class TopicController {
      *     "message": "message"
      * }
      */
-    /*
     @ResponseBody
     @PostMapping("/create")
-    public JSONObject create(@RequestBody JSONObject request) {
+    public JSONObject create(@RequestBody JSONObject request)
+            throws MissingServletRequestParameterException {
         JSONObject response = new JSONObject();
-        User user = SecurityUtil.getUser();
-        Demand demand = new Demand();
 
-        try {
-            response.put("data", topicService.addTopic(request, user, demand));
-            response.put("status", 1);
-            response.put("message", "Create Topic Success.");
-        } catch (Exception e) {
-            response.put("status", 0);
-            if (request.getString("title") == null) {
-                response.put("message", "title can't be null.");
-            } else if (request.getString("content") == null) {
-                response.put("message", "content can't be null.");
-            } else {
-                logger.error(e);
-                response.put("message", "Create Topic Failed.");
-            }
-        }
-
-        return response;
-    }
-*/
-
-    @ResponseBody
-    @PostMapping("/create")
-    public JSONObject create(@RequestBody JSONObject request) throws MissingServletRequestParameterException {
-        JSONObject response = new JSONObject();
-        User user = SecurityUtil.getUser();
-        //Demand demand = new Demand();
-        Topic topic=new Topic();
-        String title= Optional.ofNullable(
+        String title = Optional.ofNullable(
                 request.getString("title")
-        ).orElseThrow(()->new MissingServletRequestParameterException(
-                "title","String"
+        ).orElseThrow(() -> new MissingServletRequestParameterException(
+                "title", "String"
         ));
         String content = Optional.ofNullable(
                 request.getString("content")
@@ -100,36 +61,17 @@ public class TopicController {
                 "content", "String"
         ));
 
-        /*
-        Long id=request.getLong("id");
-        String title=request.getString("title");
-        String content=request.getString("content");*/
-        LocalDate today = LocalDate.now();
-        topic.setCreateTime(Timestamp.valueOf(today.atStartOfDay()));
-        topic.setCreateUser(user);
-        topic.setContent(content);
+        Topic topic = new Topic();
+
         topic.setTitle(title);
-        //topic.setId(id);
-        //topic.setDemand(demand);
-        try{
-            response.put("data", topicService.addOrUpdate(topic));
-            response.put("status", 1);
-            response.put("message", "Create Topic Success.");
-        }catch (DataIntegrityViolationException e){
-            response.put("status",0);
-            response.put("message","The topic already exists");
-        }
-        /*catch (Exception e) {
-            response.put("status", 0);
-            if (title == null) {
-                response.put("message", "title can't be null.");
-            } else if (content == null) {
-                response.put("message", "content can't be null.");
-            } else {
-                logger.error(e);
-                response.put("message", "Create Topic Failed.");
-            }
-        }*/
+        topic.setContent(content);
+        topic.setCreateUser(SecurityUtil.getUser());
+        topic.setCreateTime(new Timestamp(System.currentTimeMillis()));
+
+        response.put("data", topicService.addOrUpdate(topic));
+        response.put("status", 1);
+        response.put("message", "Create Topic Success.");
+
         return  response;
     }
 
