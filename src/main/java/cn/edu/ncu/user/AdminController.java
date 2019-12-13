7ec27d4be7002c41fa62b60b09d777366fa460cd
LinLorry <lin864464995@163.com>
2019-12-13 15:40:15 +0800

    :ok_hand: Update admin controller.

--- src/main/java/cn/edu/ncu/user/AdminController.java
+++ src/main/java/cn/edu/ncu/user/AdminController.java
@@ -3,6 +3,8 @@ package cn.edu.ncu.user;
 import cn.edu.ncu.topic.TopicService;
 import cn.edu.ncu.topic.model.Topic;
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,74 +14,83 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/admin")
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
+
     private final TopicService topicService;
+
     public AdminController(TopicService topicService) {
         this.topicService = topicService;
     }
 
-    @ResponseBody
-    @DeleteMapping
-    public JSONObject deleteTopic(@RequestParam long id){
-        JSONObject response =new JSONObject();
-        topicService.deleteById(id);
-        response.put("status",1);
-        response.put("message","Delete meeting success.");
-
-        return  response;
-    }
-    @ResponseBody
-    @GetMapping
+    @PostMapping("/boutique")
+    @Transactional
     public JSONObject addBoutique(@RequestParam long id){
         JSONObject response=new JSONObject();
-        Topic topic=topicService.loadById(id);
 
         try{
+            Topic topic = topicService.loadById(id);
+
             topic.setBoutique(true);
             topicService.addOrUpdate(topic);
-            response.put("status",1);
-            response.put("message","Set topic boutique as true");
-
+            response.put("status", 1);
+            response.put("message", "Set topic boutique as true");
         }catch (NoSuchElementException e){
-            response.put("status",0);
-            response.put("message","The topic isn't exist");
-
+            response.put("status", 0);
+            response.put("message", "The topic isn't exist");
         }
+
         return response;
     }
 
-    @ResponseBody
-    @PostMapping
+    @PostMapping("/topic")
     @Transactional
     public JSONObject update(@RequestBody JSONObject request) throws MissingServletRequestParameterException {
         JSONObject response=new JSONObject();
-        Long id=request.getLong("id");
+
         try{
-            Topic topic=topicService.loadById(id);
-            String title= Optional.ofNullable(
+            Topic topic = topicService.loadById(
+                    Optional.ofNullable(
+                            request.getLong("id")
+                    ).orElseThrow(() -> new MissingServletRequestParameterException(
+                            "id", "Long"
+                    ))
+            );
+
+            Optional.ofNullable(
                     request.getString("title")
-            ).orElseThrow(()->new MissingServletRequestParameterException(
-                    "title","String"
-            ));
-            String content = Optional.ofNullable(
+            ).ifPresent(topic::setTitle);
+
+            Optional.ofNullable(
                     request.getString("content")
-            ).orElseThrow(() -> new MissingServletRequestParameterException(
-                    "content", "String"
-            ));
-            topic.setTitle(title);
-            topic.setContent(content);
+            ).ifPresent(topic::setContent);
+
             topicService.addOrUpdate(topic);
+
             response.put("status",1);
             response.put("message","Update topic success");
-
         }catch (NoSuchElementException e){
             response.put("status",0);
             response.put("message","This topic not exist.");
-
-
         }
+
         return response;
     }
 
+    @DeleteMapping("/topic")
+    public JSONObject deleteTopic(@RequestParam long id){
+        JSONObject response =new JSONObject();
 
+        try {
+            topicService.deleteById(id);
+
+            response.put("status",1);
+            response.put("message","Delete meeting success.");
+        } catch (EmptyResultDataAccessException e) {
+            response.put("status", 0);
+            response.put("message", "This topic isn't exist.");
+        }
+
+        return  response;
+    }
 }
