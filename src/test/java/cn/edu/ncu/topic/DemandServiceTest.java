50dc7c685fd4c83ebe0cf5ffeeea76daf1755538
ZhengHaojun <740013722@qq.com>
2019-12-13 14:22:16 +0800

    Demand Service Test 添加、更新与删除测试

---
+++ src/test/java/cn/edu/ncu/topic/DemandServiceTest.java
@@ -0,0 +1,43 @@
+package cn.edu.ncu.topic;
+
+import cn.edu.ncu.topic.model.Demand;
+import cn.edu.ncu.topic.model.Topic;
+import cn.edu.ncu.topic.rep.TopicRepository;
+import net.bytebuddy.utility.RandomString;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.NoSuchElementException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemandServiceTest {
+
+    @Autowired
+    private DemandService demandService;
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @Test
+    public void addOrUpdate() {
+        Demand demand = new Demand();
+        Topic topic = topicRepository.findById(1L).orElseThrow(NoSuchElementException::new);
+
+        demand.setTopicId(1L);
+        demand.setTopic(topic);
+        demand.setContent(RandomString.make());
+        demand.setReward(0);
+
+        demandService.addOrUpdate(demand);
+        System.out.println(demand);
+    }
+
+    @Test
+    public void deleteByTopicId() {
+        demandService.deleteByTopicId(1L);
+    }
+}
